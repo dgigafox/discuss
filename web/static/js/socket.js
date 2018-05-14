e6307754c7ce7a9d6c397e8ed4f7f58d188efbf7
dgigafox <darrengegantino@gmail.com>
2018-05-15 00:11:10 +0800

    Add feature adding comment on topic

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -54,9 +54,44 @@ let socket = new Socket("/socket", {params: {token: window.userToken}})
 socket.connect()
 
 // Now that you are connected, you can join channels with a topic:
-let channel = socket.channel("comments:1", {})
-channel.join()
-  .receive("ok", resp => { console.log("Joined successfully", resp) })
-  .receive("error", resp => { console.log("Unable to join", resp) })
 
-export default socket
+const createSocket = (topicId) => {
+  let channel = socket.channel(`comments:${topicId}`, {})
+  channel.join()
+    .receive("ok", resp => {
+      renderComments(resp.comments);
+    })
+    .receive("error", resp => { console.log("Unable to join", resp) });
+
+  channel.on(`comments:${topicId}:new`, renderComment);
+
+  document.querySelector('button').addEventListener('click', () => {
+    const content = document.querySelector('textarea').value;
+
+    channel.push('comment:add', { content: content });
+  });
+}
+
+function renderComment(comment) {
+  const renderedComment = commentTemplate(comment);
+
+  document.querySelector('.collection').innerHTML += renderedComment;
+}
+
+function commentTemplate(comment) {
+  return `
+      <li class="collection-item">
+        ${comment.content}
+      </li>
+  `;
+}
+
+function renderComments(comments) {
+  const renderedComments = comments.map(comment => {
+    return commentTemplate(comment);
+});
+
+  document.querySelector('.collection').innerHTML = renderedComments.join('');
+}
+
+window.createSocket = createSocket;
